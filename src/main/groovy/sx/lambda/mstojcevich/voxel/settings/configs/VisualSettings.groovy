9b4a53bf45e0572c874914cd7d41e88ffe194008
marcusant <mstojcevich@gmail.com>
2015-03-26 21:03:11 -0400

    Disable postprocess

--- src/main/groovy/sx/lambda/mstojcevich/voxel/settings/configs/VisualSettings.groovy
+++ src/main/groovy/sx/lambda/mstojcevich/voxel/settings/configs/VisualSettings.groovy
@@ -17,7 +17,7 @@ class VisualSettings implements Serializable {
     private boolean fullscreen = false
     private int maxFPS = VoxelGame.DEBUG ? 10 : 0 //Save my battery life pls
     private int windowWidth = 640, windowHeight = 480
-    private boolean postProcess = true
+    private boolean postProcess = false
 
     public int getViewDistance() { viewDistance }
 

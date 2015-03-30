0748ee62325adea9c6f02c73b7ff8f5e800dc149
marcusant <mstojcevich@gmail.com>
2015-03-30 11:32:46 -0400

    Work on greedy meshing. Only busy-work left to go.

--- src/main/groovy/sx/lambda/mstojcevich/voxel/client/render/meshing/GreedyMesher.java
+++ src/main/groovy/sx/lambda/mstojcevich/voxel/client/render/meshing/GreedyMesher.java
@@ -1,10 +1,6 @@
 package sx.lambda.mstojcevich.voxel.client.render.meshing;
 
-import org.lwjgl.BufferUtils;
-import sx.lambda.mstojcevich.voxel.VoxelGame;
 import sx.lambda.mstojcevich.voxel.block.Block;
-import sx.lambda.mstojcevich.voxel.util.Vec3i;
-import sx.lambda.mstojcevich.voxel.world.IWorld;
 import sx.lambda.mstojcevich.voxel.world.chunk.IChunk;
 
 import java.nio.FloatBuffer;
@@ -15,6 +11,8 @@ public class GreedyMesher implements Mesher {
 
     private final IChunk chunk;
     private boolean useAlpha;
+
+    private static enum Side { TOP, BOTTOM, WEST, EAST, NORTH, SOUTH }
 
     /**
      * @param chunk Chunk to mesh
@@ -28,40 +26,104 @@ public class GreedyMesher implements Mesher {
     public MeshResult meshVoxels(Block[][][] voxels, float[][][] lightLevels) {
         List<Face> faces = new ArrayList<Face>();
 
+        // Top, bottom
         for(int y = 0; y < voxels[0].length; y++) {
-            for(int z = 0; z < voxels[0][0].length; z++) {
-                boolean[][] adMask = new boolean[voxels.length][voxels[0][0].length];
-                Block currentBlock = null;
-                float currentLL = -1;
-                int curFaceStartX = -1;
-                for (int x = 0; x < voxels.length; x++) {
-                    Block blk = voxels[x][y][z];
-                    float lightLevel = lightLevels[x][y][z];
-                    if ((currentBlock == null || currentBlock == blk)
-                            && (lightLevel == currentLL || currentLL == -1)
-                            && blk != null && !adMask[x][z]) {
-                        currentBlock = blk;
-                        currentLL = lightLevel;
-                        adMask[x][z] = true;
-                        if(curFaceStartX == -1) {
-                            curFaceStartX = x;
+            Block[][] topBlocks = new Block[voxels.length][voxels[0][0].length];
+            float[][] topLightLevels = new float[voxels.length][voxels[0][0].length];
+            Block[][] btmBlocks = new Block[voxels.length][voxels[0][0].length];
+            float[][] btmLightLevels = new float[voxels.length][voxels[0][0].length];
+            for(int x = 0; x < voxels.length; x++) {
+                for(int z = 0; z < voxels[0][0].length; z++) {
+                    if(y < voxels[0].length-1) {
+                        if(voxels[x][y+1][z] == null) {
+                            topBlocks[x][z] = voxels[x][y][z];
+                            topLightLevels[x][z] = lightLevels[x][y][z];
                         }
-                    } else { // The current face is over. Add it and start a new one
-                        faces.add(new Face(curFaceStartX, y, z, x-1, y, z+1, currentBlock, currentLL));
-                        currentBlock = null;
-                        currentLL = -1;
-                        curFaceStartX = -1;
-                        x = x-1; // Go back and try this position again, because this is a new face
+                    } else {
+                        topBlocks[x][z] = voxels[x][y][z];
+                        topLightLevels[x][z] = lightLevels[x][y][z];
                     }
+                    if(y > 0) {
+                        if(voxels[x][y-1][z] == null) {
+                            btmBlocks[x][z] = voxels[x][y][z];
+                            btmLightLevels[x][z] = lightLevels[x][y][z];
+                        }
+                    } else {
+                        btmBlocks[x][z] = voxels[x][y][z];
+                        btmLightLevels[x][z] = lightLevels[x][y][z];
+                    }
+                }
+            }
+            greedy(faces, Side.TOP, topBlocks, topLightLevels);
+            greedy(faces, Side.BOTTOM, btmBlocks, btmLightLevels);
+        }
+
+        // East, west
+        for(int x = 0; x < voxels.length; x++) {
+            Block[][] westBlocks = new Block[voxels[0][0].length][voxels[0].length];
+            float[][] westLightLevels = new float[voxels[0][0].length][voxels[0].length];
+            Block[][] eastBlocks = new Block[voxels[0][0].length][voxels[0].length];
+            float[][] eastLightLevels = new float[voxels[0][0].length][voxels[0].length];
+            for(int z = 0; z < voxels[0][0].length; z++) {
+                for(int y = 0; y < voxels[0].length; y++) {
+
                 }
             }
         }
 
-        for(Face f : faces) {
-
-        }
 
         return null;
+    }
+
+    /**
+     *
+     * @param outputList List to put faces in
+     * @param side Side being meshed
+     * @param blks Blocks on the plane
+     * @param lls Light levels of the blocks
+     */
+    private void greedy(List<Face> outputList, Side side, Block[][] blks, float lls[][]) {
+        int width = blks.length;
+        int height = blks[0].length;
+        boolean[][] used = new boolean[blks.length][blks[0].length];
+
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                Block blk = blks[x][y];
+                float ll = lls[x][y];
+                if(!used[x][y] && blk != null) {
+                    used[x][y] = true;
+                    int startX = x, endX = x+1;
+                    int startY = y, endY = y+1;
+                    while(true) {
+                        int newX = endX;
+                        if(newX == blks.length) {
+                            break;
+                        }
+                        Block newBlk = blks[newX][y];
+                        float newll = lls[newX][y];
+                        if(newBlk == blk && newll == ll && !used[x][y]) {
+                            endX++;
+                            used[x][y] = true;
+                        } else {
+                            while(true) {
+                                if(endY == height)break;
+                                boolean allPassed = true;
+                                for(int lx = startX; lx < endX; lx++) {
+                                    if(blks[lx][endY] != blk || lls[lx][endY] != ll || used[lx][endY]) {
+                                        allPassed = false;
+                                    }
+                                }
+                                if(!allPassed)break;
+                                endY++;
+                            }
+                            break;
+                        }
+                    }
+                    outputList.add(new Face(side, blk, ll, startX, startY, endX, endY));
+                }
+            }
+        }
     }
 
     @Override
@@ -75,25 +137,18 @@ public class GreedyMesher implements Mesher {
     }
 
     private static class Face {
-        private final int x1, y1, z1, x2, y2, z2;
+        private final Side side;
+        private final int x1, y1, x2, y2;
         private final Block block;
         private final float lightLevel;
 
-        public Face(int x1, int y1, int z1, int x2, int y2, int z2, Block block, float lightLevel) {
-            this.x1 = x1; this.y1 = y1; this.z1 = z1;
-            this.x2 = x2; this.y2 = y2; this.z2 = z2;
+        public Face(Side side, Block block, float lightLevel, int startX, int startY, int endX, int endY) {
             this.block = block;
             this.lightLevel = lightLevel;
+            this.x1 = startX; this.y1 = startY;
+            this.x2 = endX; this.y2 = endY;
+            this.side = side;
         }
-
-        public int getX1() { return x1; }
-        public int getX2() { return x2; }
-        public int getY1() { return y1; }
-        public int getY2() { return y2; }
-        public int getZ1() { return z1; }
-        public int getZ2() { return z2; }
-        public Block getBlock() { return block; }
-        public float getLightLevel() { return lightLevel; }
 
         public void render(FloatBuffer positions, FloatBuffer colors, FloatBuffer normals, FloatBuffer texCoords) {
 

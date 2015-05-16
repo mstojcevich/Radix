f9f59524c36040b122ab0a3d786c79a211e6d033
marcusant <mstojcevich@gmail.com>
2015-05-16 14:02:27 -0400

    Initialize libgdx project

---
+++ core/src/sx/lambda/voxel/VoxelGame.java
@@ -0,0 +1,21 @@
+package sx.lambda.voxel;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class VoxelGame extends ApplicationAdapter {
+
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+	}
+}
---
+++ desktop/src/sx/lambda/voxel/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package sx.lambda.voxel.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import sx.lambda.voxel.VoxelGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new VoxelGame(), config);
+	}
+}

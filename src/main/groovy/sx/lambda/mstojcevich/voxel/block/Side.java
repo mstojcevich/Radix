f33db5e72247e788912fa0d5473f52f48ad8d175
marcusant <mstojcevich@gmail.com>
2015-05-16 11:57:16 -0400

    Delete everything to prepare to move to the libgdx project structure

--- src/main/groovy/sx/lambda/mstojcevich/voxel/block/Side.java
+++
@@ -1,3 +0,0 @@
-package sx.lambda.mstojcevich.voxel.block;
-
-public enum Side { TOP, BOTTOM, WEST, EAST, NORTH, SOUTH }
--- src/main/groovy/sx/lambda/mstojcevich/voxel/render/NotInitializedException.groovy
+++
@@ -1,8 +0,0 @@
-package sx.lambda.mstojcevich.voxel.render
-
-/**
- * Thrown when a renderer is used when not yet initialized
- */
-class NotInitializedException extends Exception {
-
-}

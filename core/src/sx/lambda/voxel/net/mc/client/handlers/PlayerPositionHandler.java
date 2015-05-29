6915aee8f9ce730e76ac228562498a0768e217f2
Marcus Stojcevich <mstojcevich@gmail.com>
2015-05-29 13:42:26 -0400

    Listen to server's rotations again

--- core/src/sx/lambda/voxel/net/mc/client/handlers/PlayerPositionHandler.java
+++ core/src/sx/lambda/voxel/net/mc/client/handlers/PlayerPositionHandler.java
@@ -14,6 +14,6 @@ public class PlayerPositionHandler implements PacketHandler<ServerPlayerPosition
     @Override
     public void handle(ServerPlayerPositionRotationPacket packet) {
         game.getPlayer().getPosition().setPos((float)packet.getX(), (float)packet.getY(), (float)packet.getZ());
-        //game.getPlayer().getRotation().setRot(360-packet.getPitch(), 180-packet.getYaw());
+        game.getPlayer().getRotation().setRot(360-packet.getPitch(), 180-packet.getYaw());
     }
 }

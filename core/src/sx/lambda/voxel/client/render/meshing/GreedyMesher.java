e460d05716f9774c25649570b1e80fed9f5c1fa9
Marcus Stojcevich <mstojcevich@gmail.com>
2015-05-30 10:11:26 -0400

    Use curBlock instead of voxels[x][y][z] for readability

--- core/src/sx/lambda/voxel/client/render/meshing/GreedyMesher.java
+++ core/src/sx/lambda/voxel/client/render/meshing/GreedyMesher.java
@@ -45,19 +45,20 @@ public class GreedyMesher implements Mesher {
             float[][] btmLightLevels = new float[voxels.length][voxels[0][0].length];
             for (int x = 0; x < voxels.length; x++) {
                 for (int z = 0; z < voxels[0][0].length; z++) {
-                    if (voxels[x][y][z] == null) continue;
+                    Block curBlock = voxels[x][y][z];
+                    if (curBlock == null) continue;
                     if (y < voxels[0].length - 1) {
-                        if (!ocCond.shouldOcclude(voxels[x][y][z], voxels[x][y + 1][z])) {
-                            topBlocks[x][z] = voxels[x][y][z].getID();
+                        if (!ocCond.shouldOcclude(curBlock, voxels[x][y + 1][z])) {
+                            topBlocks[x][z] = curBlock.getID();
                             topLightLevels[x][z] = lightLevels[x][y + 1][z];
                         }
                     } else {
-                        topBlocks[x][z] = voxels[x][y][z].getID();
+                        topBlocks[x][z] = curBlock.getID();
                         topLightLevels[x][z] = 1;
                     }
                     if (y > 0) {
-                        if(!ocCond.shouldOcclude(voxels[x][y][z], voxels[x][y - 1][z])) {
-                            btmBlocks[x][z] = voxels[x][y][z].getID();
+                        if(!ocCond.shouldOcclude(curBlock, voxels[x][y - 1][z])) {
+                            btmBlocks[x][z] = curBlock.getID();
                             btmLightLevels[x][z] = lightLevels[x][y - 1][z];
                         }
                     } else {
@@ -78,15 +79,16 @@ public class GreedyMesher implements Mesher {
             float[][] eastLightLevels = new float[voxels[0][0].length][voxels[0].length];
             for (int z = 0; z < voxels[0][0].length; z++) {
                 for (int y = 0; y < voxels[0].length; y++) {
-                    if (voxels[x][y][z] == null) continue;
+                    Block curBlock = voxels[x][y][z];
+                    if (curBlock == null) continue;
 
                     int westNeighborX = chunk.getStartPosition().x + x - 1;
                     IChunk westNeighborChunk = chunk.getWorld().getChunkAtPosition(westNeighborX, chunk.getStartPosition().z + z);
                     if (westNeighborChunk != null) {
                         Block westNeighborBlk = VoxelGameAPI.instance.getBlockByID(
                                 westNeighborChunk.getBlockIdAtPosition(westNeighborX, y, z));
-                        if (!ocCond.shouldOcclude(voxels[x][y][z], westNeighborBlk)) {
-                            westBlocks[z][y] = voxels[x][y][z].getID();
+                        if (!ocCond.shouldOcclude(curBlock, westNeighborBlk)) {
+                            westBlocks[z][y] = curBlock.getID();
                             westLightLevels[z][y] = westNeighborChunk.getLightLevel(westNeighborX, y, z);
                         }
                     } else {
@@ -99,8 +101,8 @@ public class GreedyMesher implements Mesher {
                     if (eastNeighborChunk != null) {
                         Block eastNeighborBlk = VoxelGameAPI.instance.getBlockByID(
                                 eastNeighborChunk.getBlockIdAtPosition(eastNeighborX, y, z));
-                        if (!ocCond.shouldOcclude(voxels[x][y][z], eastNeighborBlk)) {
-                            eastBlocks[z][y] = voxels[x][y][z].getID();
+                        if (!ocCond.shouldOcclude(curBlock, eastNeighborBlk)) {
+                            eastBlocks[z][y] = curBlock.getID();
                             eastLightLevels[z][y] = eastNeighborChunk.getLightLevel(eastNeighborX, y, z);
                         }
                     } else {
@@ -122,7 +124,8 @@ public class GreedyMesher implements Mesher {
             float[][] southLightLevels = new float[voxels.length][voxels[0].length];
             for (int x = 0; x < voxels.length; x++) {
                 for (int y = 0; y < voxels[0].length; y++) {
-                    if (voxels[x][y][z] == null) continue;
+                    Block curBlock = voxels[x][y][z];
+                    if (curBlock == null) continue;
                     int northNeighborZ = chunk.getStartPosition().z + z + 1;
                     int southNeighborZ = chunk.getStartPosition().z + z - 1;
                     IChunk northNeighborChunk = chunk.getWorld().getChunkAtPosition(chunk.getStartPosition().x + x, northNeighborZ);
@@ -131,8 +134,8 @@ public class GreedyMesher implements Mesher {
                     if (northNeighborChunk != null) {
                         Block northNeighborBlock = VoxelGameAPI.instance.getBlockByID(
                                 northNeighborChunk.getBlockIdAtPosition(x, y, northNeighborZ));
-                        if (!ocCond.shouldOcclude(voxels[x][y][z], northNeighborBlock)) {
-                            northBlocks[x][y] = voxels[x][y][z].getID();
+                        if (!ocCond.shouldOcclude(curBlock, northNeighborBlock)) {
+                            northBlocks[x][y] = curBlock.getID();
                             northLightLevels[x][y] = northNeighborChunk.getLightLevel(x, y, northNeighborZ);
                         }
                     } else {
@@ -143,8 +146,8 @@ public class GreedyMesher implements Mesher {
                     if (southNeighborChunk != null) {
                         Block southNeighborBlock = VoxelGameAPI.instance.getBlockByID(
                                 southNeighborChunk.getBlockIdAtPosition(x, y, southNeighborZ));
-                        if (!ocCond.shouldOcclude(voxels[x][y][z], southNeighborBlock)) {
-                            southBlocks[x][y] = voxels[x][y][z].getID();
+                        if (!ocCond.shouldOcclude(curBlock, southNeighborBlock)) {
+                            southBlocks[x][y] = curBlock.getID();
                             southLightLevels[x][y] = southNeighborChunk.getLightLevel(x, y, southNeighborZ);
                         }
                     } else {

29e4e4f205719a8fe7657b1079929d5c83af046b
Marcus Stojcevich <mstojcevich@gmail.com>
2015-05-31 00:43:49 -0400

    Give chunk its own hashCode function

--- core/src/sx/lambda/voxel/world/chunk/Chunk.java
+++ core/src/sx/lambda/voxel/world/chunk/Chunk.java
@@ -546,4 +546,12 @@ public class Chunk implements IChunk {
         VoxelGameAPI.instance.getEventManager().push(new EventChunkRender(Chunk.this));
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + this.startPosition.x;
+        hash = 71 * hash + this.startPosition.z;
+        return hash;
+    }
+
 }

1ef445df95a21f647c4f8672e291fe92a6cfe47e
Marcus Stojcevich <mstojcevich@gmail.com>
2015-06-03 23:44:02 -0400

    Don't clear color buffer, don't set viewport every frame

--- core/src/sx/lambda/voxel/VoxelGameClient.java
+++ core/src/sx/lambda/voxel/VoxelGameClient.java
@@ -312,9 +312,7 @@ public class VoxelGameClient extends ApplicationAdapter {
     }
 
     private void prepareNewFrame() {
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        Gdx.gl.glClearColor(0.4f, 0.6f, 1.0f, 1.0f);
-        Gdx.gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        Gdx.gl.glClear(GL_DEPTH_BUFFER_BIT);
     }
 
     public void updateSelectedBlock() {
@@ -591,6 +589,8 @@ public class VoxelGameClient extends ApplicationAdapter {
 
     @Override
     public void resize(int width, int height) {
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
         hudCamera.setToOrtho(false, width, height);
         hudCamera.update();
         guiBatch.setProjectionMatrix(hudCamera.combined);

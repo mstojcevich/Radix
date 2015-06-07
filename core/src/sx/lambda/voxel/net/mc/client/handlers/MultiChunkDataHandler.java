c429505964acfccd6012788733bb8d5dcb17308e
Marcus Stojcevich <mstojcevich@gmail.com>
2015-06-06 23:30:39 -0400

    Don't spawn a new thread for processing chunk data

--- core/src/sx/lambda/voxel/net/mc/client/handlers/MultiChunkDataHandler.java
+++ core/src/sx/lambda/voxel/net/mc/client/handlers/MultiChunkDataHandler.java
@@ -20,73 +20,72 @@ public class MultiChunkDataHandler implements PacketHandler<ServerMultiChunkData
 
     @Override
     public void handle(final ServerMultiChunkDataPacket packet) {
-        new Thread() {
-            @Override
-            public void run() {
-                for(int column = 0; column < packet.getColumns(); column++) {
-                    int cx = packet.getX(column)*16;
-                    int cz = packet.getZ(column)*16;
-                    int biomeID = packet.getBiomeData(column)[0];
-                    Biome biome = VoxelGameAPI.instance.getBiomeByID(biomeID);
-                    if(biome == null)
-                        biome = VoxelGameAPI.instance.getBiomeByID(biomeID-128);
-                    if(biome == null)
-                        biome = VoxelGameAPI.instance.getBiomeByID(0);
-                    IChunk ck = game.getWorld().getChunk(cx, cz);
-                    if(ck != null) {
-                        game.getWorld().rmChunk(ck);
-                    }
-                    ck = new sx.lambda.voxel.world.chunk.Chunk(game.getWorld(), new Vec3i(cx, 0, cz), biome, false);
-                    int cy = 0;
-                    for(Chunk c : packet.getChunks(column)) {
-                        if(c == null) {
-                            for (int x = 0; x < 16; x++) {
-                                for (int z = 0; z < 16; z++) {
-                                    for (int y = 0; y < 16; y++) {
-                                        try {
-                                            ck.setSunlight(x, cy+y, z, 15);
-                                        } catch (CoordinatesOutOfBoundsException e) {
-                                            e.printStackTrace();
-                                        }
-                                    }
-                                }
-                            }
-                            cy += 16;
-                            continue;
-                        }
-                        for(int x = 0; x < 16; x++) {
-                            for(int z = 0; z < 16; z++) {
-                                for(int y = 0; y < 16; y++) {
-                                    try {
-                                        int id = c.getBlocks().getBlock(x, y, z);
-                                        short meta = (short) c.getBlocks().getData(x, y, z);
-                                        byte blocklight = (byte) c.getBlockLight().get(x, y, z);
-                                        byte skylight = (byte) c.getSkyLight().get(x, y, z);
-                                        boolean blockExists = false;
-                                        if (id <= 0 || VoxelGameAPI.instance.getBlockByID(id) != null) {
-                                            blockExists = true;
-                                        }
-                                        if (!blockExists) {
-                                            id = BuiltInBlockIds.UNKNOWN_ID;
-                                        }
-                                        if (id > 0)
-                                            ck.setBlock(id, x, cy + y, z, false);
-                                        if (meta > 0)
-                                            ck.setMeta(meta, x, cy + y, z);
-                                        ck.setBlocklight(x, cy+y, z, blocklight);
-                                        ck.setSunlight(x, cy+y, z, skylight);
-                                        ck.finishAddingSun();
-                                    } catch (CoordinatesOutOfBoundsException ex) {
-                                        ex.printStackTrace();
-                                    }
-                                }
-                            }
-                        }
-                        cy += 16;
-                    }
-                    game.getWorld().addChunk(ck);
-                }
+        for(int column = 0; column < packet.getColumns(); column++) {
+            int cx = packet.getX(column)*16;
+            int cz = packet.getZ(column)*16;
+            int biomeID = packet.getBiomeData(column)[0];
+
+            Biome biome = VoxelGameAPI.instance.getBiomeByID(biomeID);
+            if(biome == null)
+                biome = VoxelGameAPI.instance.getBiomeByID(biomeID-128);
+            if(biome == null)
+                biome = VoxelGameAPI.instance.getBiomeByID(0);
+
+            IChunk ck = game.getWorld().getChunk(cx, cz);
+            if(ck != null) {
+                game.getWorld().rmChunk(ck);
             }
-        }.start();
+
+            ck = new sx.lambda.voxel.world.chunk.Chunk(game.getWorld(), new Vec3i(cx, 0, cz), biome, false);
+            int cy = 0;
+            for(Chunk c : packet.getChunks(column)) {
+                if(c == null) {
+                    for (int x = 0; x < 16; x++) {
+                        for (int z = 0; z < 16; z++) {
+                            for (int y = 0; y < 16; y++) {
+                                try {
+                                    ck.setSunlight(x, cy+y, z, 15);
+                                } catch (CoordinatesOutOfBoundsException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                    cy += 16;
+                    continue;
+                }
+                for(int x = 0; x < 16; x++) {
+                    for(int z = 0; z < 16; z++) {
+                        for(int y = 0; y < 16; y++) {
+                            try {
+                                int id = c.getBlocks().getBlock(x, y, z);
+                                short meta = (short) c.getBlocks().getData(x, y, z);
+                                byte blocklight = (byte) c.getBlockLight().get(x, y, z);
+                                byte skylight = (byte) c.getSkyLight().get(x, y, z);
+                                boolean blockExists = false;
+                                if (id <= 0 || VoxelGameAPI.instance.getBlockByID(id) != null) {
+                                    blockExists = true;
+                                }
+                                if (!blockExists) {
+                                    id = BuiltInBlockIds.UNKNOWN_ID;
+                                }
+                                if (id > 0)
+                                    ck.setBlock(id, x, cy + y, z, false);
+                                if (meta > 0)
+                                    ck.setMeta(meta, x, cy + y, z);
+                                ck.setBlocklight(x, cy+y, z, blocklight);
+                                ck.setSunlight(x, cy+y, z, skylight);
+                                ck.finishAddingSun();
+                            } catch (CoordinatesOutOfBoundsException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    }
+                }
+                cy += 16;
+            }
+
+            game.getWorld().addChunk(ck);
+        }
     }
 }

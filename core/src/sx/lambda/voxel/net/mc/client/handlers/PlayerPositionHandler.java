cc57a8113473cd278c2845ecd23d424ed6c7f8d4
Marcus Stojcevich <mstojcevich@gmail.com>
2015-06-06 21:46:58 -0400

    Fix pitch in PlayerPositionHandler

--- core/src/sx/lambda/voxel/net/mc/client/handlers/PlayerPositionHandler.java
+++ core/src/sx/lambda/voxel/net/mc/client/handlers/PlayerPositionHandler.java
@@ -14,6 +14,6 @@ public class PlayerPositionHandler implements PacketHandler<ServerPlayerPosition
     @Override
     public void handle(ServerPlayerPositionRotationPacket packet) {
         game.getPlayer().getPosition().set((float)packet.getX(), (float)packet.getY(), (float)packet.getZ());
-        game.getPlayer().getRotation().setRot(360-packet.getPitch(), 180-packet.getYaw());
+        game.getPlayer().getRotation().setRot(-packet.getPitch(), 180-packet.getYaw());
     }
 }
